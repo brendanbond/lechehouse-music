5409d87157a7f6c7a43af78005a7d8ff637bb45f
Brendan Bond <brendanbond@gmail.com>
2020-09-04 12:28:42 -0500

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,95 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    createDefaultProgram: true,
+    project: './tsconfig.json',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', '@typescript-eslint', 'react-hooks'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
+    ],
+    'jsx-a11y/label-has-associated-control': [
+      2,
+      {
+        labelComponents: ['label'],
+        labelAttributes: ['htmlFor'],
+        controlComponents: ['input'],
+      },
+    ],
+    'react/prefer-stateless-function': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/destructuring-assignment': 'off',
+    'react/no-unused-state': 'off',
+    'no-restricted-globals': 'off',
+    'no-useless-constructor': 'off',
+    'react/jsx-boolean-value': 'off',
+    'react/state-in-constructor': 'off',
+    'react/jsx-curly-newline': 'off',
+    'react/jsx-wrap-multilines': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
+    'no-console': 1,
+    'react/prop-types': 'off',
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    'import/default': 'off',
+    'import/extensions': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+      },
+    ],
+    'import/no-unresolved': [2, { caseSensitive: false }],
+    'no-nested-ternary': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.test.ts', '*.test.tsx', '**/testUtils.tsx', 'src/jest/*.ts'],
+      rules: {
+        'import/no-extraneous-dependencies': 0,
+      },
+    },
+  ],
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+};
