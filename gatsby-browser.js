62f27b062848e392ac37f4f79912a75e9b932297
Brendan Bond <brendanbond@gmail.com>
2020-10-04 13:27:13 -0500

    simple media query for snap scrolling on mobile

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,20 +4,28 @@ import { Global, css } from '@emotion/core';
 
 import betoTheme from './theme';
 
-export const wrapRootElement = ({ element }) => (
-  <ThemeProvider theme={betoTheme}>
-    <CSSReset />
-    <Global
-      styles={css`
-        html {
-          scroll-snap-type: y mandatory;
-          scroll-padding: 136px;
-        }
-        body {
-          scroll-snap-type: y mandatory;
-        }
-      `}
-    />
-    {element}
-  </ThemeProvider>
-);
+export const wrapRootElement = ({ element }) => {
+  return (
+    <ThemeProvider theme={betoTheme}>
+      <CSSReset />
+      <Global
+        styles={css`
+          html,
+          body {
+            scroll-snap-type: y mandatory;
+            scroll-padding: 136px;
+          }
+
+          @media only screen and (max-width: 991px) {
+            html,
+            body {
+              scroll-snap-type: y mandatory;
+              scroll-padding: 92px;
+            }
+          }
+        `}
+      />
+      {element}
+    </ThemeProvider>
+  );
+};
